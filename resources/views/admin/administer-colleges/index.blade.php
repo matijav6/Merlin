0fc4ed52a972cc863636abb2790f371c1ff1ed20
Matija Vuk <matija.vuk.97@gmail.com>
2017-12-25 17:54:08 +0100

    visual changes

--- resources/views/admin/administer-colleges/index.blade.php
+++ resources/views/admin/administer-colleges/index.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            @include('admin.sidebar')
-
-            <div class="col-md-9">
+        <div class="row">        
+        <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Colleges</div>
                     <div class="panel-body">
